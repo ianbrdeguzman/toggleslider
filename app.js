cfa87d2399b41c33a5bd7906144ac5a2cc6f2fe7
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-02-17 15:01:43 -0500

    added change background color

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const checkbox = document.querySelector('.toggle-btn');
 checkbox.addEventListener('click', () => {
   const toggle = document.querySelector('.toggle');
   const circle = document.querySelector('.circle');
+  const body = document.querySelector('body');
+  const github = document.querySelector('#github');
+
   if(checkbox.checked) {
     console.log('checked')
     circle.classList.toggle('checked');
-    toggle.style.backgroundColor = 'green';
+    circle.innerHTML = '🌕';
+    toggle.style.backgroundColor = '#2ecc71';
+    body.style.backgroundColor = '#b8c6db';
+    body.style.backgroundImage = 'linear-gradient(315deg, #b8c6db 0%, #f5f7fa 74%)'
+    github.style.color = '#272727';
   } else {
     console.log('not checked')
     circle.classList.toggle('checked');
+    circle.innerHTML = '🌑';
     toggle.style.backgroundColor = 'grey';
+    body.style.backgroundColor = '#485461';
+    body.style.backgroundImage = 'linear-gradient(315deg, #485461 0%, #28313b 74%)'
+    github.style.color = '#f5f5f5';
   }
 });
